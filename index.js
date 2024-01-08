76d7d061175fe611c1ead11422c4b7a0107e459e
giovankes <hello@giotje.dev>
2024-01-08 17:22:50 +0100

    messages from discord websocket gateway are now displayed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import Wrapper from './client.js';
+import WebSocket from 'ws';
 import { settings } from './config/config.json' assert { type: 'json' };
 const { token } = process.env;
 const self = new Wrapper(token, settings);
@@ -9,4 +10,12 @@ self.on('ready', () => {
     `Logged in as ${self.client.user.username}#${self.client.user.discriminator}`,
     '\x1b[0m'
   );
+});
+
+// const ws = new WebSocket(process.env.ws);
+
+self.on('message_create', (m) => {
+  if (m.webhook_id) return;
+
+  console.log(m);
 });
